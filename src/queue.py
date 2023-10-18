0ebcdcb4f75db66cd0cbd1a9304ea24ebcd78615
mikhail <mikhail5861@gmail.com>
2023-10-18 19:47:24 +0300

    hw3

--- src/queue.py
+++ src/queue.py
@@ -7,7 +7,8 @@ class Node:
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Queue:
@@ -15,7 +16,9 @@ class Queue:
 
     def __init__(self):
         """Конструктор класса Queue"""
-        pass
+        self.head = None
+        self.tail = None
+        self.all = []
 
     def enqueue(self, data):
         """
@@ -23,7 +26,17 @@ class Queue:
 
         :param data: данные, которые будут добавлены в очередь
         """
-        pass
+        self.head = Node(data, self.head)
+        self.all.append(data)
+
+    def revers_enqueue(self):
+        if self.tail is None:
+            if self.head is None:
+                return None
+            while self.head is not None:
+                pop = self.head
+                self.head = self.head.next_node
+                self.tail = Node(pop.data, self.tail)
 
     def dequeue(self):
         """
@@ -31,8 +44,10 @@ class Queue:
 
         :return: данные удаленного элемента
         """
-        pass
+        self.revers_enqueue()
+        return self.tail.data
 
     def __str__(self):
         """Магический метод для строкового представления объекта"""
-        pass
+
+        return '\n'.join(self.all) if self.tail is not None else ''
--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием unittest для модуля queue."""
+import unittest
+from src.queue import Node, Queue
+
+
+class TestStack(unittest.TestCase):
+
+    def test_Node(self):
+        self.assertEqual(Node(1, None).data, 1)
+        self.assertEqual(Node(None, 4).next_node, 4)
+
+    def test_pop(self):
+        queue_test = Queue()
+        queue_test.enqueue('data')
+        queue_test.enqueue('something')
+
+        self.assertEqual(queue_test.dequeue(), 'data')
+        self.assertEqual(queue_test.tail.next_node.data, 'something')
+
